82ea02f9d7417c9610dc4fca51764ece252edb76
Zachary Sain <zachsain@gmail.com>
2021-11-02 13:21:20 -0500

    commit tree 3c144902a8898a7ff482d125dbea439a337a87d1

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+
+var customerName = "bob"
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer(bestCustomer) {
+    window.bestCustomer = "not bob"
+    return bestCustomer 
+}
+
+function overwriteBestCustomer(newCustomer) {
+    return window.bestCustomer = newCustomer 
+}
+
+function changeLeastFavoriteCustomer(unsuccesful)  {
+    const leastFavorite = "whatever"
+    leastFavorite = unsuccesful 
+}
